dfcb1be029309a29dcdaa6ba8e1320093d443a29
Leoanrd Polz <mail@leonardpolz.de>
2024-06-07 13:22:00 +0200

    Added explicit depends on for vnet module

--- tf-az-virtual-network/main.tf
+++ tf-az-virtual-network/main.tf
@@ -96,7 +96,10 @@ resource "azurerm_subnet_network_security_group_association" "network_security_g
   subnet_id                 = azurerm_subnet.subnets[each.key].id
   network_security_group_id = module.network_security_groups.network_security_groups[each.key].id
 
-  depends_on = [module.network_security_groups]
+  depends_on = [
+    module.network_security_groups,
+    azurerm_subnet.subnets
+  ]
 }
 
 locals {
